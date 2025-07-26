7ee7b96ad9d51da88fab34bd07271e35ae9980ee
Luiz Togni <contato.togni@gmail.com>
2025-07-25 21:57:38 -0300

    feat: add delete method

--- src/database.js
+++ src/database.js
@@ -1,41 +1,53 @@
-import fs from 'node:fs/promises';
 import { randomUUID } from "node:crypto";
+import fs from "node:fs/promises";
 
 const databasePath = new URL("../db.json", import.meta.url);
 
 export class Database {
-    #database = {};
+  #database = {};
 
-    constructor() {
-        fs.readFile(databasePath, "utf8")
-            .then(data => {
-                this.#database = JSON.parse(data);
-            })
-            .catch(() => {
-                this.#persist();
-            });
-    }
-
-    #persist() {
-        fs.writeFile(databasePath, JSON.stringify(this.#database));
-    }
-
-    select(table) {
-        const data = this.#database[table] ?? []; 
-        return data;
-    }
-
-    insert(table, data) {
-        const id = randomUUID();
-
-        if (Array.isArray(this.#database[table])) {
-            this.#database[table].push({...data, id});
-            this.#persist();
-            return data;
-        }
-
-        this.#database[table] = [{...data, id}];
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
         this.#persist();
-        return data;
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+    return data;
+  }
+
+  insert(table, data) {
+    const id = randomUUID();
+
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push({ ...data, id });
+      this.#persist();
+      return data;
     }
+
+    this.#database[table] = [{ ...data, id }];
+    this.#persist();
+    return data;
+  }
+
+  delete(table, id) {
+    if (Array.isArray(this.#database[table])) {
+      const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+      if (rowIndex > -1) {
+        this.#database[table].splice(rowIndex, 1);
+        this.#persist();
+        return;
+      }
+    }
+  }
 }
